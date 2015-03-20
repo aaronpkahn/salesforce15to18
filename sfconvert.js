bcb1899f744a34fcb1f806612fdc13c71e0b1e2e
Aaron Kahn <akahn@solarcity.com>
2015-03-19 19:10:09 -0700

    changed port

--- sfconvert.js
+++ sfconvert.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var url = require('url');
-var port = 8092;
+var port = (process.env.PORT || 8092);
 
 var server = http.createServer(function(req,res){
   var query = url.parse(req.url,true).query;
